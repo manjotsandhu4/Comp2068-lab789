154b630b384f10bf0607ce527bf311a86544e8b9
manjotsandhu4 <manjot.sandhu@ymail.com>
2020-08-02 21:55:20 -0400

    finaas

--- app.js
+++ app.js
@@ -75,9 +75,9 @@ app.get('/test', (req, res) => {
   res.status(200).json({message: 'Hello World'});
 });
 
-app.use(express.static(path.join(__dirname, '/client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 app.get('*', (req, res)=>{
-  res.sendFile(path.join(__dirname, '/client/build.index.html'));
+  res.sendFile(path.join(__dirname+'/client/build.index.html'));
 })
 
 // Start our server
